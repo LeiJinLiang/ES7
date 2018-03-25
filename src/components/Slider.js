fabd21431c0f4b5d59c4875bfcd1902d602b9324
466179464@qq.com <466179464@qq.com>
2018-03-25 23:33:27 +0800

    rude completed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,73 +2,6 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 
-function Slider1( element ) {
-    this.el = document.querySelector( element );
-    this.init();
-}
-
-Slider1.prototype = {
-    init: function() {
-        this.links = this.el.querySelectorAll( "#slider-nav a" );
-        this.wrapper = this.el.querySelector( "#slider-wrapper" );
-        this.navigate();
-    },
-    navigate: function() {
-
-        for( var i = 0; i < this.links.length; ++i ) {
-            var link = this.links[i];
-            this.slide( link );
-        }
-    },
-
-    animate: function( slide ) {
-        var parent = slide.parentNode;
-        var caption = slide.querySelector( ".caption" );
-        var captions = parent.querySelectorAll( ".caption" );
-        for( var k = 0; k < captions.length; ++k ) {
-            var cap = captions[k];
-            if( cap !== caption ) {
-                cap.classList.remove( "visible" );
-            }
-        }
-        caption.classList.add( "visible" );
-    },
-
-    slide: function( element ) {
-        var self = this;
-        element.addEventListener( "click", function( e ) {
-            e.preventDefault();
-            var a = this;
-            self.setCurrentLink( a );
-            var index = parseInt( a.getAttribute( "data-slide" ), 10 ) + 1;
-            var currentSlide = self.el.querySelector( ".slide:nth-child(" + index + ")" );
-
-            self.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
-            self.animate( currentSlide );
-
-        }, false);
-    },
-    setCurrentLink: function( link ) {
-        var parent = link.parentNode;
-        var a = parent.querySelectorAll( "a" );
-
-        link.className = "current";
-
-        for( var j = 0; j < a.length; ++j ) {
-            var cur = a[j];
-            if( cur !== link ) {
-                cur.className = "";
-            }
-        }
-    }
-};
-
-document.addEventListener( "DOMContentLoaded", function() {
-    // var aSlider = new Slider1( "#slider" );
-
-});
-
-
 
 const Wrapper = styled.div`
     margin: 2em auto;
@@ -143,7 +76,9 @@ class Slider extends Component{
                 {text : 'Caption3', bg : 'cadetblue'},
                 {text : 'Caption4', bg : 'chocolate'},
                 {text : 'Caption5', bg : 'pink'},
-            ]
+            ],
+            count : 0,
+            timer : ''
         }
     }
 
@@ -152,7 +87,7 @@ class Slider extends Component{
     }
 
     renderItem = (item,idx) => (
-        <Item bg = {item.bg} key = {idx} ref = {'warpper'+idx}>
+        <Item bg = {item.bg} key = {idx}>
             <Img>{idx+1}</Img>
             <Text>Caption{idx+1}</Text>
         </Item>
@@ -160,14 +95,32 @@ class Slider extends Component{
 
 
     handleClick = (idx) => {
-        
+        if (this.search) {
+            this.search.style.left = -(960*idx)+'px'
+        }
+    }
+
+    handleMove = () => {
+        if (this.search && this.state.count < 5) {
+            console.log(2);
+            this.search.style.left = -(960*this.state.count)+'px'
+            this.state.count = this.state.count + 1;
+        }else{
+            this.search.style.left = '0px';
+            this.state.count = 0;
+        }
+
+    }
+    componentDidMount() {
+        const _self = this
+       this.state.timer = setInterval(_self.handleMove,1000);
     }
 
     render(){
         const { data } = this.state
         return(
             <Wrapper>
-                <Content>
+                <Content innerRef={(search) => { this.search = search }}>
                     {data.map((item,idx)=>(
                         this.renderItem(item,idx)
                     ))}
